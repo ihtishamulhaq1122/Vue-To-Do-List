070b72b1e1d80b7637badbe8a849a461597cbf4f
Ehtisham ul Haq <ulhaqehtisham80@gmail.com>
2023-05-23 05:07:42 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+ 
+<TASK v-bind:tasks="tasks"></TASK>
+
+
+
+</template>
+
+<script>
+
+import TASK from './components/ToDOlist.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    TASK,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue JS",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Watch netflix",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Go shopping",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Learn guitar",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Send email",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+ 
+
+ 
+ /* eslint-disable */
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
